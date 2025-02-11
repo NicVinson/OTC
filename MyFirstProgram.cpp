43fdc3391092475a0af89fdb9cd06016d93b5eec
Nic Vinson <nicvinson05@outlook.com>
2025-02-10 19:17:43 -0600

    First program

---
+++ MyFirstProgram.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    //Declation and assigning of variables
+    int myInt = 42;
+    float myFloat = 3.14;
+    double myDouble = 2.71828;
+    char myChar = 'C';
+    bool myBool = true; //will output one for true
+    std::string myString = "Hello,World!\n";
+
+    //Output of variables
+    std::cout << "\nInteger: " << myInt << "\nFloat: " << myFloat << "\nDouble: " << myDouble << "\nChar: " << myChar << "\nBool: " << myBool << "\nString: " << myString << "\n";
+
+    return 0;
+}
