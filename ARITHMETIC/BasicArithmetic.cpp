5a5e1893eac29e9b5a4ea389f3337ae5d83c4585
Nic Vinson <nicvinson05@outlook.com>
2025-02-13 19:26:18 -0600

    Created a readme as well as the program in which the different basic arithmetic operations are preformed and outputed

---
+++ ARITHMETIC/BasicArithmetic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    //variables for calculations
+    int a = 100;
+    int b = 3;
+    //output
+    std::cout << a << " + " << b << " = " << a + b << "\n";
+    std::cout << a << " - " << b << " = " << a - b << "\n";
+    std::cout << a << " / " << b << " = " << a / b << "\n";
+    std::cout << a << " * " << b << " = " << a * b << "\n";
+    std::cout << a << " % " << b << " = " << a % b;
+
+    return 0;
+}
---
+++ ARITHMETIC/README.md
@@ -0,0 +1,5 @@
+Nicholas Vinson 2/13/2025
+
+-------------------------
+
+This program outputs basic arithmetic operations such as +,-,/,*,%
