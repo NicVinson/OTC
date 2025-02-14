112e95a4255f97fb3f1d51075a668e8b565fb1b2
Nic Vinson <nicvinson05@outlook.com>
2025-02-13 19:05:24 -0600

    Added a greeting output that includes the users name

--- GREETING/Greeting.cpp
+++ GREETING/Greeting.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+
 int main(){
+    //String variable made to save the users name
     std::string usersName;
     //Prompt the user to enter their name
-    std::cout << "Hello, what is your name? ";
+    std::cout << "\nHello, what is your name? ";
     std::cin >> usersName;
+    //Output a greeting along with the users name
+    std::cout << "\nIt's nice to meet you, " << usersName;
 
-    std::cout << usersName;
+
+    return 0;
 }
