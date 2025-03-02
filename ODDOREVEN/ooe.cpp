48d6f6870d9ef5ac1e1a8b3337cbf28d856edff1
Nic Vinson <nicvinson05@outlook.com>
2025-03-02 14:24:53 -0600

    Changed output and prompt formating

--- ODDOREVEN/ooe.cpp
+++ ODDOREVEN/ooe.cpp
@@ -5,14 +5,14 @@ int main(){
     //variable for saving the users number
     int userNum;
     //prompt user for a whole number and save it to userNum
-    std::cout << "\nPlease enter a whole number: ";
+    std::cout << "\nEnter a whole number: ";
     std::cin >> userNum;
     //If else statement that determines if the number is odd or even by dividing the number by 2 and determining if the number has a remainder
     if(userNum % 2 == 0){
-        std::cout << "\nYour number is even!\n";
+        std::cout << userNum << " is a even number.\n";
     } 
     else{
-        std::cout << "\nYour number is odd!\n";
+        std::cout << userNum << " is a odd number.\n";
     }
     
     std::cout << "\n";
