b12e58cbcbc8ae5085a655d1a547ef5326a722d5
Nic Vinson <nicvinson05@outlook.com>
2025-03-02 14:14:36 -0600

    completed the program as well as added documentation to the program

--- ODDOREVEN/README.md
+++ ODDOREVEN/README.md
@@ -3,3 +3,10 @@ Nicholas Vinson 03/02/2025
 --------------------------
 
 This program takes a user inputed integer(whole number) and uses the modulo operator to detrmine if the integer is evenly divisible.
+
+
+
+Test Cases:
+2 ---- Even
+52054 ---- Even
+30245 ---- Odd
--- ODDOREVEN/ooe.cpp
+++ ODDOREVEN/ooe.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
 int main(){
+
+    //variable for saving the users number
     int userNum;
-
-    std::cout << "Please enter a whole number: ";
+    //prompt user for a whole number and save it to userNum
+    std::cout << "\nPlease enter a whole number: ";
     std::cin >> userNum;
-
-    if(userNum % 0){
-        std::cout << "Your number is even!\n";
+    //If else statement that determines if the number is odd or even by dividing the number by 2 and determining if the number has a remainder
+    if(userNum % 2 == 0){
+        std::cout << "\nYour number is even!\n";
     } 
     else{
-        std::cout << "Your number is odd!\n";
+        std::cout << "\nYour number is odd!\n";
     }
     
-
+    std::cout << "\n";
     return 0;
 }
