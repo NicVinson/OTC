13ceb4216ca8dab4705691804d12ed303c7aece4
Nic Vinson <nicvinson05@outlook.com>
2025-03-28 10:33:02 -0500

    Created the prompt to ask the user for the launch time, and ran a test to make sure the variable is saved

--- COUNTINGDOWN/takeoff.cpp
+++ COUNTINGDOWN/takeoff.cpp
@@ -2,11 +2,14 @@
 
 int main(){
     //Ask user for a number, then save the number to be used later
-
-
+    std::cout << "Thank you for joining the space program!\nToday we will be launching a rocket and we need your help.\nPlease enter a launch time that is greater than 1\n";
+    std::cout << "\nPlease enter here: ";
+    int launchtime;
+    std::cin >> launchtime;
+    std::cout << launchtime;
     //If statement that check if the user entered 0 or anything less than 0
 
-    
+
 
     //Use a for loop that takes the users number and subtracts 1 until the number reaches 1 and outputs the numbers
 
