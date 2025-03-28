1b4afe3da66df0329647dbe844eb567dfaa03480
Nic Vinson <nicvinson05@outlook.com>
2025-03-28 10:53:15 -0500

    Created an outline of the code

---
+++ FRUITLOOPS/fruitsorting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    //Declare an arry with 5 fruits
+
+    //Ask user to input 2 fruits into the array
+
+    //Use a loop to print out each fruit from the array
+}
