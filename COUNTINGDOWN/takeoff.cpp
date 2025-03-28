6dd7b889d937adf3c022f72adc2f736854c05f98
Nic Vinson <nicvinson05@outlook.com>
2025-03-28 10:45:49 -0500

    Completed the for loop that subtracts from the launchtime and outputs the seconds going down and after the time hits 1 blast off is outputted

--- COUNTINGDOWN/takeoff.cpp
+++ COUNTINGDOWN/takeoff.cpp
@@ -6,17 +6,20 @@ int main(){
     std::cout << "\nPlease enter the integer here: ";
     int launchtime;
     std::cin >> launchtime;
-    std::cout << "\n\n";
+    std::cout << "\n";
     //If statement that check if the user entered 0 or anything less than 0
     if(launchtime < 1){
         std::cout << "Error...Error\n \nPlease try again.";
         return 0;
     }
 
-
     //Use a for loop that takes the users number and subtracts 1 until the number reaches 1 and outputs the numbers
-
+    for(int x = launchtime; x >= 1; x--){
+        std::cout << launchtime << "\n";
+        launchtime--;
+    }
 
 
     //output that states "Blast off!"
+    std::cout << "Blast Off!!!";
 }
