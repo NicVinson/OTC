4e74f17cd36ce29b611c7c898adc8753c61f4227
Nic Vinson <nicvinson05@outlook.com>
2025-03-28 10:27:57 -0500

    Created the takeoff file, and andded the structure for the code

---
+++ COUNTINGDOWN/takeoff.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    //Ask user for a number, then save the number to be used later
+
+
+    //If statement that check if the user entered 0 or anything less than 0
+
+    
+
+    //Use a for loop that takes the users number and subtracts 1 until the number reaches 1 and outputs the numbers
+
+
+
+    //output that states "Blast off!"
+}
