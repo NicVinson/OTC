c04060bf1cfae249a766482879ecf768af436180
Nic Vinson <nicvinson05@outlook.com>
2025-03-28 10:38:28 -0500

    Created the if statement that checks the number entered is less than 1

--- COUNTINGDOWN/takeoff.cpp
+++ COUNTINGDOWN/takeoff.cpp
@@ -2,13 +2,16 @@
 
 int main(){
     //Ask user for a number, then save the number to be used later
-    std::cout << "Thank you for joining the space program!\nToday we will be launching a rocket and we need your help.\nPlease enter a launch time that is greater than 1\n";
-    std::cout << "\nPlease enter here: ";
+    std::cout << "\nThank you for joining the space program!\nToday we will be launching a rocket and we need your help.\nPlease enter a launch time that is greater than or equal to 1\n";
+    std::cout << "\nPlease enter the integer here: ";
     int launchtime;
     std::cin >> launchtime;
-    std::cout << launchtime;
+    std::cout << "\n\n";
     //If statement that check if the user entered 0 or anything less than 0
-
+    if(launchtime < 1){
+        std::cout << "Error...Error\n \nPlease try again.";
+        return 0;
+    }
 
 
     //Use a for loop that takes the users number and subtracts 1 until the number reaches 1 and outputs the numbers
