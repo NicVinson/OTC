e2a6d0908ac278e3bb5fcc9a2ecd070850187929
Nic Vinson <nicvinson05@outlook.com>
2025-04-17 20:54:44 -0500

    Basic outline for part 1 completed

---
+++ LITTLEWHILE/towhileornottowhile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(){
+    //create a random number generator that is 1 to 10 inclusive
+    //prompt user to enter a number and save it
+    //create a while loop that checks if the entered number is the same as the rand num(if not keep going until it is)
+    //output if user gets the number correct
+
+}
