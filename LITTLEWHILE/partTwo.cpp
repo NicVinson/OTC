7258d52ef48b6b841b8d3d46ae1a2e84c080184f
Nic Vinson <nicvinson05@outlook.com>
2025-04-17 21:39:07 -0500

    Completed part two, aslo created a test case page for part two, that takes the users input and saves it so I can compare answers and check the system is functioning properly.

--- LITTLEWHILE/partTwo.cpp
+++ LITTLEWHILE/partTwo.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include <fstream>
 
 using std::cout;
 using std::cin;
 using std::string;
+using std::ofstream;
 
 int main(){
+    ofstream outputFile("testCaseP2.txt", std::ios::app);
     //Create a password to be used for guessing purposes
+    string secPass = "Mechanical-or-Software-Engineering-1";
     //Prompt user for password
+    cout << "\nPlease enter the password: ";
+    string userPass;
+    cin >> userPass;
+    outputFile << userPass << "\n";
     //Use a while loop to check for correct password
+    while (secPass != userPass){
+        cout << "\nPlease try again: ";
+        cin >> userPass;
+        outputFile << userPass;
+        outputFile << "\n";
+    }
     //output a welcome message
+    cout << "\nWelcome to indecision!\n";
+
+    
 }
