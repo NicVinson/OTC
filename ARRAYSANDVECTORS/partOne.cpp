0a065103db176bb744bb8e13598d737e879adbcc
Nic Vinson <nicvinson05@outlook.com>
2025-04-24 13:39:02 -0500

    Finished p1

--- ARRAYSANDVECTORS/partOne.cpp
+++ ARRAYSANDVECTORS/partOne.cpp
@@ -1,6 +1,22 @@
 //Figure out what libraries need to be used
-//Use the using functionality to make the coding simpler and easier to read
+#include <iostream>
+
+using namespace std;
+
+void printArr(const int arr[], int size){
+    for (int a = 0; a < size; ++a){
+        cout << arr[a] << " ";
+    }
+    cout << "\n";
+}
+int main(){
 //Declare an array of 5 values
+    int mainArr[5] = {1, 2, 4, 10, 12};
 //Print the array
+    printArr(mainArr, 5);
 //Modify 2 of the elements(We will be doing 1 and 3)
+    mainArr[1] = 3;
+    mainArr[3] = 11;
 //Print the updated array
+    printArr(mainArr, 5);
+}
