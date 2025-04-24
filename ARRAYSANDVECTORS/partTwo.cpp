bf13ea45fa427f95d1769e1d018aa5f5f2e59a44
Nic Vinson <nicvinson05@outlook.com>
2025-04-24 08:21:43 -0500

    Completed part two using all vector functions

--- ARRAYSANDVECTORS/partTwo.cpp
+++ ARRAYSANDVECTORS/partTwo.cpp
@@ -1,6 +1,62 @@
 //Figure out what libraries to use
-//Use the using functionality for ease of coding
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//Created a function to print out vectors(I didn't want to retype a for loop every time)
+void printVec(const vector<int>& vec){
+    for(int a : vec){
+        cout << a << " ";
+    }
+    cout << "\n";
+}
+
+int main(){
 //Declare a vector of integers
+vector<int> theInts = {2, 5, 7};
+
 //Print the original vector using a loop.
-//Use the vector functions
-//Print two new versions of the vector one with appending functions, and one with the clear function
+cout << "Original Vector: \n";
+printVec(theInts);
+
+//Clear Function
+cout << "\nAfter clear()\n";
+theInts.clear();  //Clears entire vector
+printVec(theInts);
+
+//Push back function
+cout << "\nAfter push_back(5)\n";
+theInts.push_back(5);  //Traditionally adds the 5 to the back of the vector, but as I cleared the vector it will be the only value
+printVec(theInts);
+
+//Insert Function
+cout << "\nAfter using insert(theInts.begin(), 6)\n";
+theInts.insert(theInts.begin(), 6);  //Inserts the value into the 0 index
+printVec(theInts);
+
+//Size function
+cout << "\nVector Size\n";
+int vecSize = theInts.size();  //Counts total index values and then outputs how many values are in the vector
+cout << vecSize << "\n";
+
+//Pop back function
+cout << "\nAfter pop_back()\n";
+theInts.pop_back();  //Drops the last index from the vector
+printVec(theInts);
+
+//At function
+//I added some more values so I could specify an index to print out
+theInts.push_back(7);
+theInts.insert(theInts.begin()+ 1, 9);
+cout << "\nVector additions: ";
+printVec(theInts);
+cout << "Value AT index 1\n";
+int vecInd = theInts.at(1);  //Gets the indexed value
+cout << vecInd << "\n";
+
+//Erase function
+cout << "\nAfter erase(theInts.begin() + 1): \n";
+theInts.erase(theInts.begin() + 1);  //Erases a specified index
+printVec(theInts);
+}
