6eb58908155a64a5c8a82a632892e0ed09c02b6c
NicVinson <nicvinson05@outlook.com>
2025-05-05 16:57:02 -0500

    Completed the habitat and diet class-objects to be used in the animal abstract class later on

--- DIGITALZOO/digiZoo.cpp
+++ DIGITALZOO/digiZoo.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
 
-class Animal{
-private:
-public:
+//Create the Diet Object
+class Diet{
+    public:
+        string foodPref;
+        Diet(const string& foodPref){
+            this->foodPref = foodPref;
+        }
+        void dietDescriber() const{
+            cout << "My Diet: " << foodPref << "\n";
+        }
 };
+//Create the Habitat Object
+class Habitat{
+    public:
+        string mainHab;
+        Habitat(const string& mainHab){
+            this->mainHab = mainHab;
+        }
+        void habitatDescriber(){
+            cout << "My Home: " << mainHab << "\n";
+        }
+};
+//Create the animal abstract class
+    //include the make sound function
+    //Create variables for name and age, as well as species(use a const string in the contructor for the final )
 
 
-int main(){
 
-}
