ad5a1e006fe23fd2bbf897bc619a908b5b3c6ac9
NicVinson <nicvinson05@outlook.com>
2025-05-05 21:03:44 -0500

    Set up the animal class with what I need to do, including protected variables and the public constructor as well as preparing to creat the animal sounds

--- DIGITALZOO/digiZoo.cpp
+++ DIGITALZOO/digiZoo.cpp
@@ -26,6 +26,18 @@ class Habitat{
         }
 };
 //Create the animal abstract class
+class Animal{
+    /*
+    Create protected variables should include the name and age and species, 
+    as well as the diet and habitat this is because we still want to be able to 
+    motify some of them but also protect the variables that we dont want to change
+    I am also to lazy to seperate private and protected and protected technically does both
+    */
+    /*
+    Create the public systems, creating the constructor for the animals,
+    also creating the animal sounds
+    */
+};
     //include the make sound function
     //Create variables for name and age, as well as species(use a const string in the contructor for the final )
 
