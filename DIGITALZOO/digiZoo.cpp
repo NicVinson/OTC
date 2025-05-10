a452f2eef8d7a4f839db94359dbb7eaf377191e3
NicVinson <nicvinson05@outlook.com>
2025-05-09 21:41:39 -0500

    I am still confused but you know what we have some functionality. Once I am done I will go through and see what does and does not need to be there

--- DIGITALZOO/digiZoo.cpp
+++ DIGITALZOO/digiZoo.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 //Create the Diet Object
 class Diet{
     public:
+        
         string foodPref;
+        Diet(){
+            foodPref;
+        }
         Diet(const string& foodPref){
             this->foodPref = foodPref;
         }
@@ -16,8 +20,12 @@ class Diet{
 };
 //Create the Habitat Object
 class Habitat{
+    
     public:
         string mainHab;
+        Habitat(){
+            mainHab;
+        }
         Habitat(const string& mainHab){
             this->mainHab = mainHab;
         }
@@ -39,37 +47,54 @@ class Animal{
         const string animalType;
         Diet animalDiet;
         Habitat animalHabitat;
+
     /*
     Create the public systems, creating the constructor for the animals,
     also creating the animal sounds
     */
     public:
-        Animal(const string& animalName, int animalAge, const string& animalType, const Diet& animalDiet, const Habitat& animalHabitat) 
-        : animalType(animalType), animalDiet(animalDiet), animalHabitat(animalHabitat){
+        Animal(string animalName, int animalAge, string animalType, Diet& animalDiet, Habitat animalHabitat) 
+        : animalType(animalType){
             this->animalName = animalName;
             this->animalAge = animalAge;
+            this->animalDiet = animalDiet;
+            this->animalHabitat = animalHabitat;
         }
     // make animal sounds object
-    virtual void animalSound() const = 0;
-    virtual void animalEating() const = 0;
+        virtual void animalSound() const = 0;
+        virtual void animalEating() const = 0;
 
     //display info function this will be final as once its made there should be no changing
-    virtual void animalDisplay() const final{
-        cout << "Name: " << animalName << "\n";
-        cout << "Age: " << animalAge << "\n";
-        cout << "Species: " << animalType << "\n";
-        cout << "Diet: " << animalDiet.getDiet() << "\n";
-        cout << "Habitat: " << animalHabitat.getHabitat() << "\n";
-    }   
-    virtual ~Animal(){}
+        virtual void animalDisplay() const final{
+            cout << "Name: " << animalName << "\n";
+            cout << "Age: " << animalAge << "\n";
+            cout << "Species: " << animalType << "\n";
+            cout << "Diet: " << animalDiet.getDiet() << "\n";
+            cout << "Habitat: " << animalHabitat.getHabitat() << "\n";
+        }   
+        virtual ~Animal(){}
 };
-
+class Lion: public Animal{
+    public:
+        Lion(string name, int age, Diet diet, Habitat habitat)
+        :Animal(name, age, "Lion", diet, habitat){}
+    void animalSound() const override{
+        cout << animalName << "Meow's\n";
+    }
+    void animalEating() const override{
+        cout << animalName << "NOMNOM\n";
+    }
+};
 int main(){
     Diet carn("Carnivore");
-   
-    //testing diet functionality
-    cout << "Diet test: " << carn.getDiet();
+    Habitat grass("Grasslands");
+
+    Lion tester("Pickles", 9, carn, grass);
+    tester.animalSound();
+    tester.animalEating();
+    tester.animalDisplay();
 }
+
 
 
 
