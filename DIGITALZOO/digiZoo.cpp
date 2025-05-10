b6b096d805cabf8d1297a52e9fa050628619f3a3
NicVinson <nicvinson05@outlook.com>
2025-05-09 20:22:55 -0500

    I am a little confused with what exactly I am doing now so I am rredrawing for what subclasses I will be adding, but I have tested the functionality of the diet class and I bbelieve that it works, and if that works then hab would work

--- DIGITALZOO/digiZoo.cpp
+++ DIGITALZOO/digiZoo.cpp
@@ -10,8 +10,8 @@ class Diet{
         Diet(const string& foodPref){
             this->foodPref = foodPref;
         }
-        void dietDescriber() const{
-            cout << "My Diet: " << foodPref << "\n";
+        string getDiet() const{
+            return foodPref;
         }
 };
 //Create the Habitat Object
@@ -21,8 +21,8 @@ class Habitat{
         Habitat(const string& mainHab){
             this->mainHab = mainHab;
         }
-        void habitatDescriber(){
-            cout << "My Home: " << mainHab << "\n";
+        string getHabitat() const{
+            return mainHab;
         }
 };
 //Create the animal abstract class
@@ -33,13 +33,43 @@ class Animal{
     motify some of them but also protect the variables that we dont want to change
     I am also to lazy to seperate private and protected and protected technically does both
     */
+    protected:
+        string animalName;
+        int animalAge;
+        const string animalType;
+        Diet animalDiet;
+        Habitat animalHabitat;
     /*
     Create the public systems, creating the constructor for the animals,
     also creating the animal sounds
     */
+    public:
+        Animal(const string& animalName, int animalAge, const string& animalType, const Diet& animalDiet, const Habitat& animalHabitat) 
+        : animalType(animalType), animalDiet(animalDiet), animalHabitat(animalHabitat){
+            this->animalName = animalName;
+            this->animalAge = animalAge;
+        }
+    // make animal sounds object
+    virtual void animalSound() const = 0;
+    virtual void animalEating() const = 0;
+
+    //display info function this will be final as once its made there should be no changing
+    virtual void animalDisplay() const final{
+        cout << "Name: " << animalName << "\n";
+        cout << "Age: " << animalAge << "\n";
+        cout << "Species: " << animalType << "\n";
+        cout << "Diet: " << animalDiet.getDiet() << "\n";
+        cout << "Habitat: " << animalHabitat.getHabitat() << "\n";
+    }   
+    virtual ~Animal(){}
 };
-    //include the make sound function
-    //Create variables for name and age, as well as species(use a const string in the contructor for the final )
+
+int main(){
+    Diet carn("Carnivore");
+   
+    //testing diet functionality
+    cout << "Diet test: " << carn.getDiet();
+}
 
 
 
